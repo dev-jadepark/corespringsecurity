faaad4992e356bf1da45b182107b889ae893d0f7
parkforte <parkforte@naver.com>
2022-05-03 07:46:49 +0900

    initial commit

--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -13,14 +13,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated()
 
-        http
-                .formLogin()
-                ;
-
-        http
-                .csrf();
-
+        .and()
+                .formLogin();
     }
 }
